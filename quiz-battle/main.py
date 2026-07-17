01ed7f3694290a8cf3388eaae4caf04762ddd377
kopher <kopher1601@gmail.com>
2026-07-17 15:07:21 +0900

    WebSocket Broadcasting

--- quiz-battle/main.py
+++ quiz-battle/main.py
@@ -3,14 +3,32 @@ from starlette.websockets import WebSocket, WebSocketDisconnect
 
 app = FastAPI()
 
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: list[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+manager = ConnectionManager()
+
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
+    await manager.connect(websocket)
 
     try:
         while True:
             message = await websocket.receive_text()
-            await websocket.send_text(f"Server received: {message}")
+            await manager.broadcast(message)
     except WebSocketDisconnect:
+        manager.disconnect(websocket)
         print("WebSocket connection closed")
